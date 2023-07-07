a205228578e102c88296ad19a4f59048849cba24
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-07 10:54:59 -0500

    complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badge_messages = [badge_maker(name) for name in names]
+    return badge_messages
 
 def assign_rooms(names):
-    return None
+    room_list = [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]
+    return room_list
 
 def printer(names):
-    return None
+    badge_messages = batch_badge_creator(names)
+    for badge_message in badge_messages:
+        print(badge_message) 
+        
+    assign_list = assign_rooms(names)
+    for assign in assign_list:
+        print(assign)
